5c6660c1e708d3cc5eac1ed17c92e0c68e49edfe
wlin <wlin@guidewire.com>
2013-04-05 17:43:25 -0400

    Initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KumonApp::Application.config.secret_token = '6ecb49448fc313a90a2b9e062070b5feddea6bc8945ef22311215135ce15ba0a74711eeb03bf5e0c285ad70013d6aa4b42f2e85a59d5d24e9b5af17f867278e2'
