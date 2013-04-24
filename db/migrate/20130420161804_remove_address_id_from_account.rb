b5174a920fc9f48e3e784143344c95c8a1468add
wlin <wlin@guidewire.com>
2013-04-24 17:10:27 -0400

    remove migration

--- db/migrate/20130420161804_remove_address_id_from_account.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAddressIdFromAccount < ActiveRecord::Migration
-  def up
-    remove_column :accounts, :address_id
-  end
-
-  def down
-    add_column :accounts, :address_id, :integer
-  end
-end
